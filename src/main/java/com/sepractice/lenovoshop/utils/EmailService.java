56cffecfa78148cd36430fb259f5275bb38e6a9f
安小白 <3401797899@qq.com>
2024-08-30 20:06:36 +0800

    fix: 修复上一个提交的依赖错误问题

--- src/main/java/com/sepractice/lenovoshop/utils/EmailService.java
+++ src/main/java/com/sepractice/lenovoshop/utils/EmailService.java
@@ -1,26 +1,33 @@
 package com.sepractice.lenovoshop.utils;
 
 import jakarta.annotation.Resource;
+import jakarta.mail.internet.InternetAddress;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Service
-public class SendEmailUtil {
+@Component
+public class EmailService{
     @Autowired
-    private static JavaMailSender javaMailSender;
+    private JavaMailSender javaMailSender;
 
-    public static void sendEmail(String to, String subject, String content) {
+    public void sendEmail(String to, String subject, String content) {
         // 发送邮件
         SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            message.setFrom(String.valueOf(new InternetAddress("3401797899@qq.com", "LenovoShop", "UTF-8")));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
-        message.setTo(to);
-        message.setSentDate(new Date());
         javaMailSender.send(message);
     }
 }
