9fcd878838b13089e37810146ee162edf0d99247
Mo01iHt <1328273623@qq.com>
2024-09-01 23:28:31 +0800

    feat: 新增ProductService

--- src/main/java/com/sepractice/lenovoshop/service/ProductService.java
+++ src/main/java/com/sepractice/lenovoshop/service/ProductService.java
@@ -2,6 +2,7 @@ package com.sepractice.lenovoshop.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sepractice.lenovoshop.entity.Product;
+import com.sepractice.lenovoshop.entity.ProductConfig;
 import com.sepractice.lenovoshop.mapper.CategoryMapper;
 import com.sepractice.lenovoshop.mapper.ProductConfigMapper;
 import com.sepractice.lenovoshop.mapper.ProductMapper;
@@ -17,7 +18,7 @@ public class ProductService {
     @Autowired
     private ProductMapper productMapper;
     @Autowired
-    private ProductConfigMapper productConfigMapper;
+    private ProductConfigMapper configMapper;
     @Autowired
     private CategoryMapper categoryMapper;
 
@@ -27,4 +28,29 @@ public class ProductService {
         queryWrapper.eq("category_id", categoryId);
         return productMapper.selectList(queryWrapper);
     }
+
+    public List<ProductConfig> getConfigsFromProduct(Long productId) {
+        QueryWrapper<ProductConfig> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("product_id", productId);
+        return configMapper.selectList(queryWrapper);
+    }
+
+    public Long getIdByCode(Long productCode) {
+        QueryWrapper<ProductConfig> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("product_code", productCode);
+
+        // 使用 selectOne 方法查找符合条件的单个记录
+        ProductConfig config = configMapper.selectOne(queryWrapper);
+
+        // 返回找到的 config_id，如果没有找到则返回 null
+        return config.getProductId().longValue();
+    }
+
+    public String getConfigByCode(Long productCode) {
+        QueryWrapper<ProductConfig> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("product_code", productCode);
+        return configMapper.selectOne(queryWrapper).getValue();
+    }
+
+    public String get
 }
