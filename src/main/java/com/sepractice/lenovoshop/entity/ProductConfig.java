a06460c3979bb76fd2052a5a5d989264b904a177
Mo01iHt <1328273623@qq.com>
2024-09-04 17:30:04 +0800

    fix: 修改详情页返回内容

--- src/main/java/com/sepractice/lenovoshop/entity/ProductConfig.java
+++ src/main/java/com/sepractice/lenovoshop/entity/ProductConfig.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.List;
@@ -15,8 +16,10 @@ import java.util.List;
 @TableName(value = "configs", autoResultMap = true)
 public class ProductConfig {
     @TableId(value = "id", type = IdType.AUTO)
+    @JsonIgnore
     private Integer id;
 
+    @JsonIgnore
     private Integer productId;
 
     private String name;
